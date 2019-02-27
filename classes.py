721207f4e36560b972de7e555e49f9b19f740247
Lena S <46871956+lsstenvi@users.noreply.github.com>
2019-02-27 11:24:03 -0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,32 @@
+#!/lsstenvi/bin/python
+
+import tkinter as tk
+tk.TkVersion
+print("hello World!")
+class User:
+	def __init__(self,username,isOnline):
+		self.username = username
+		self.isOnline = isOnline
+
+class Host(User):
+	def __init__(self,name,isOnline):
+		User.__init__(self,username,isOnline)
+
+
+person = User()
+person.username = "lsstenvi"
+person.isOnline = True
+
+print(person.username)
+print(person.isOnline)
+
+dude = Host()
+dude.username = "hostDude"
+dude.isOnline = True
+
+
+top = tk.Tk()
+
+top.title("First GUI")
+
+top.mainloop()
