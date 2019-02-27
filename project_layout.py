940ba7278fd59996d9b4b258adcb82abea453cb7
Kristoffer Vollset <kriss_vollset@hotmail.com>
2019-02-27 12:52:57 -0500

    Update project_layout.py

--- project_layout.py
+++ project_layout.py
@@ -1,7 +1,7 @@
 from tkinter import*
 root=Tk()
 root.configure(background="green")
-root.geometry("700x400")
+root.geometry("500x300")
 
 aboveFrame = Frame(root,bg="green")
 aboveFrame.pack(fill=X)
@@ -10,8 +10,8 @@ middleFrame.place(x=0,y=100)
 label=Label(aboveFrame,text="Spotify_ sync",fg="black",bg="green")
 label.grid(ipadx=2,ipady=2,sticky=N+W)
 
-buttomFrame=Frame(root,bg="white")
-buttomFrame.place(x=0,y=200)
+buttomFrame=Frame(root,bg="black")
+buttomFrame.place(x=0,y=220)
 
 
 button_1=Button(middleFrame,text="app_1",width=10, bg="white")
@@ -23,12 +23,12 @@ button_3.grid(row=1,padx=5,pady=5)
 button_4=Button(middleFrame,text="app_4",width=10, bg="white")
 button_4.grid(row=1,column=1,padx=5,pady=5)
 
-label_1=Label(buttomFrame,text="Currently playing: ",bg="white")
-label_2=Label(buttomFrame,text="Song: X, Artist: Y, Year: Y",bg="white")
-label_3=Label(buttomFrame,text="Duration: mm:ss",bg="white")
-label_1.grid(sticky=W)
-label_2.grid(sticky=W)
-label_3.grid(sticky=W)
+label_1=Label(buttomFrame,text="Currently playing: ",bg="black",  fg="white", anchor=E, font=("Helvetica",13,"bold", "italic"))
+label_2=Label(buttomFrame,text="Song: X, Artist: Y, Year: Y",bg="black",fg="white",width=25)
+label_3=Label(buttomFrame,text="Duration: mm:ss",bg="black",fg="white",width=25)
+label_1.pack()
+label_2.pack()
+label_3.pack()
 
 
 root.mainloop()
