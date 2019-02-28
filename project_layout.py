98cebe71cfcbfe09bcee3eb1804a2a36fb7b4ff1
Kristoffer Vollset <kriss_vollset@hotmail.com>
2019-02-28 18:30:55 -0500

    Update project_layout.py

--- project_layout.py
+++ project_layout.py
@@ -1,7 +1,8 @@
 from tkinter import*
+
 root=Tk()
 root.configure(background="green")
-root.geometry("500x300")
+root.geometry("600x300")
 
 aboveFrame = Frame(root,bg="green")
 aboveFrame.pack(fill=X)
@@ -10,25 +11,39 @@ middleFrame.place(x=0,y=100)
 label=Label(aboveFrame,text="Spotify_ sync",fg="black",bg="green")
 label.grid(ipadx=2,ipady=2,sticky=N+W)
 
-buttomFrame=Frame(root,bg="black")
-buttomFrame.place(x=0,y=220)
+bottomFrame=Frame(root,bg="black")
+bottomFrame.place(x=0,y=220)
+bottomLeftFrame=Frame(root,bg="black")
+bottomLeftFrame.place(x=300,y=220)
 
 
-button_1=Button(middleFrame,text="app_1",width=10, bg="white")
-button_1.grid(row=0,padx=5,pady=5)
-button_2=Button(middleFrame,text="app_2",width=10, bg="white")
-button_2.grid(row=0,column=1,padx=5,pady=5)
-button_3=Button(middleFrame,text="app_3",width=10, bg="white")
-button_3.grid(row=1,padx=5,pady=5)
-button_4=Button(middleFrame,text="app_4",width=10, bg="white")
-button_4.grid(row=1,column=1,padx=5,pady=5)
 
-label_1=Label(buttomFrame,text="Currently playing: ",bg="black",  fg="white", anchor=E, font=("Helvetica",13,"bold", "italic"))
-label_2=Label(buttomFrame,text="Song: X, Artist: Y, Year: Y",bg="black",fg="white",width=25)
-label_3=Label(buttomFrame,text="Duration: mm:ss",bg="black",fg="white",width=25)
-label_1.pack()
-label_2.pack()
-label_3.pack()
+botton_1=Button(middleFrame,text="app_1",width=10, bg="white")
+botton_1.grid(row=0,padx=5,pady=5)
+botton_2=Button(middleFrame,text="app_2",width=10, bg="white")
+botton_2.grid(row=0,column=1,padx=5,pady=5)
+botton_3=Button(middleFrame,text="app_3",width=10, bg="white")
+botton_3.grid(row=1,padx=5,pady=5)
+botton_4=Button(middleFrame,text="app_4",width=10, bg="white")
+botton_4.grid(row=1,column=1,padx=5,pady=5)
+
+label_1=Label(bottomFrame,text="Currently playing",bg="black",  fg="white", anchor=E, font=("Helvetica",12,"bold", "italic"))
+label_2=Label(bottomFrame,text="Song: X, Artist: Y, Year: Y",bg="black",fg="white",width=35)
+label_3=Label(bottomFrame,text="Duration: mm:ss",bg="black",fg="white",width=35)
+label_4=Label(bottomLeftFrame,text="Do you want to change song to ",bg="black",  fg="white", anchor=E,font=("Helvetica",12,"bold", "italic"))
+label_5=Label(bottomLeftFrame,text="Song: X, Artist: Y, Year: Y",bg="black",fg="white",width=35)
+label_6=Label(bottomLeftFrame,text="Yes, I want to change to this song song",bg="black",fg="white",width=35)
+cYes=Checkbutton(bottomLeftFrame)
+cNo=Checkbutton(bottomLeftFrame)
+
+
+label_2.grid(row=2,ipadx=5,ipady=2)
+label_3.grid(row=1,ipadx=5)
+label_1.grid(row=0,ipadx=5)
+label_4.grid(columnspan=2)
+label_5.grid()
+label_6.grid()
+cYes.grid(row=2,column=1,columnspan=1)
 
 
 root.mainloop()
