2a1012476093351cff4115aff694e7e6dd670732
Kristoffer Vollset <kriss_vollset@hotmail.com>
2019-03-20 13:31:27 -0400

    Update project_layout.py

--- project_layout.py
+++ project_layout.py
@@ -1,45 +1,113 @@
 from tkinter import*
 
+def apps():
+    mainFrameVote.place_forget()
+    mainFrameGroup.place_forget()
+    mainFrameProfile.place_forget()
+    mainFrameApp.place(x=0,y=25,relheight="1",relwidth="0.884")
+    middleFrame.pack()
+    rps=PhotoImage(file="rps.png")
+    botton_1=Button(middleFrame,image=rps,compound=TOP)
+    botton_1.image=rps
+    botton_1.grid(row=0,padx=10,pady=10)
+    botton_2=Button(middleFrame,text="app_2",width=10, bg="white")
+    botton_2.grid(row=0,column=1,padx=10,pady=10)
+    botton_3=Button(middleFrame,text="app_3",width=10, bg="white")
+    botton_3.grid(row=1,padx=10,pady=10)
+    botton_4=Button(middleFrame,text="app_4",width=10, bg="white")
+    botton_4.grid(row=1,column=1,padx=10,pady=10)
+
+    
+def votes():
+    lblVote=Label(mainFrameVote,text="This is the vote room",font=("Helvetica",12,"bold", "italic"))
+    mainFrameGroup.place_forget()
+    mainFrameApp.place_forget()
+    mainFrameProfile.place_forget()
+    mainFrameVote.place(x=0,y=25,relheight="1",relwidth="0.884")
+    lblVote.pack()
+
+
+def profiles():
+    lblProfile=Label(mainFrameProfile,text="This room will show some user \n information from Spotify profile",font=("Helvetica",12,"bold", "italic"))
+    mainFrameGroup.place_forget()
+    mainFrameApp.place_forget()
+    mainFrameVote.place_forget()
+    mainFrameProfile.place(x=0,y=25,relheight="1",relwidth="0.884")
+    lblProfile.pack()
+def groups():
+    lblGroups=Label(mainFrameGroup,text="This room will show who's in the group, \n and give options for leaving the group \n or add new members to the group",font=("Helvetica",12,"bold", "italic"))
+    mainFrameVote.place_forget()
+    mainFrameApp.place_forget()
+    mainFrameProfile.place_forget()
+    mainFrameGroup.place(x=0,y=25,relheight="1",relwidth="0.884")
+    lblGroups.pack()
+
+def backToMusic():
+    mainFrameApp.place_forget()
+    mainFrameVote.place_forget()
+    mainFrameProfile.place_forget()
+    mainFrameGroup.place_forget()
+    
+    
 root=Tk()
 root.configure(background="green")
 root.geometry("350x260")
+root.maxsize(350,260)
+root.title('Spotify sync')
+
+
+
 
 aboveFrame = Frame(root,bg="green")
-aboveFrame.pack(fill=X)
-settings=PhotoImage(file="settingsIcon.png")
-btnSettings=Button(aboveFrame,image=settings,fg="black", bg="green")
-label=Label(aboveFrame,text="Spotify_ sync_logo",fg="black",bg="green")
-label.grid(ipadx=105,ipady=2,sticky=N+W)
-btnSettings.grid(row=0,column=2,sticky=E)
+aboveFrame.pack(side=TOP,fill=X)
+profile=PhotoImage(file="profileIcon.png")
+btnProfile=Button(aboveFrame,image=profile,fg="black", bg="green",command=profiles)
+lblProfile=Label(aboveFrame,text="Spotify_ sync_logo",fg="black",bg="green")
+btnProfile.pack(side=RIGHT,ipadx=5)
+lblProfile.pack()
+
+menubar = Menu(btnProfile)
+setting = Menu(menubar, tearoff=0)
+setting.add_command(label="something")
+setting.add_command(label="something")
+setting.add_separator()
+setting.add_command(label="Log out", command=root.quit)
+menubar.add_cascade(label="Settings", menu=setting)
+
+
+
+
+
+
+
+# display the menu
+root.config(menu=menubar)
+
+#label.grid(ipadx=105,ipady=2,sticky=N+W)
+#btnSettings.grid(row=0,column=2,sticky=E)
 
 bottomLeftFrame=Frame(root,bg="white")
 menyFrame=Frame(root,bg="green")
-menyFrame.pack(side=RIGHT)
+menyFrame.pack(side=RIGHT,fill=Y)
 bottomLeftFrame.pack(fill=X)
 bottomRightFrame=Frame(root,bg="black")
 bottomRightFrame.pack(fill=X)
 
+#Frames for our app-layout
+mainFrameApp=Frame(root,bg="green")
+middleFrame=Frame(mainFrameApp,bg="black")
 
-mainFrame=Frame(root,bg="green")
-middleFrame=Frame(mainFrame,bg="black")
-def self():
-    mainFrame.place(x=0,y=25,relheight="1",relwidth="0.884")
-    middleFrame.grid()
-    rps=PhotoImage(file="rps.png")
-    botton_1=Button(middleFrame,image=rps,compound=TOP)
-    botton_1.image=rps
-    botton_1.grid(row=0,padx=5,pady=5)
-    botton_2=Button(middleFrame,text="app_2",width=10, bg="white")
-    botton_2.grid(row=0,column=1,padx=5,pady=5)
-    botton_3=Button(middleFrame,text="app_3",width=10, bg="white")
-    botton_3.grid(row=1,padx=5,pady=5)
-    botton_4=Button(middleFrame,text="app_4",width=10, bg="white")
-    botton_4.grid(row=1,column=1,padx=5,pady=5)
-    
+#Frame for our vote-layout
+mainFrameVote=Frame(root,bg="green")
 
-def backToMusic():
-    mainFrame.place_forget()
-    middleFrame.grid_forget()
+#Frame for our group-layout
+mainFrameGroup=Frame(root,bg="green")
+#Frame for our profile-layout
+mainFrameProfile=Frame(root,bg="green")
+
+
+
+
     
 
 
@@ -52,12 +120,12 @@ app=PhotoImage(file="appIcon.png")
 group=PhotoImage(file="groupIcon.png")
 btnMusic=Button(menyFrame,text="music",image=musicSmall,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green", command=backToMusic)
 btnMusic.grid()
-btnApp=Button(menyFrame,text="app", image=app,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green",command=self)
+btnApp=Button(menyFrame,text="app", image=app,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green",command=apps)
 btnApp.grid()
-btnVote=Button(menyFrame,text="vote", image=vote,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green")
+btnVote=Button(menyFrame,text="vote", image=vote,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green",command=votes)
 btnVote.grid()
-btnVote=Button(menyFrame,text="group", image=group,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green")
-btnVote.grid()
+btnGroup=Button(menyFrame,text="group", image=group,compound=TOP,borderwidth=2,relief="groove",fg="black", bg="green",command=groups)
+btnGroup.grid()
 
 
 label_1=Label(bottomLeftFrame,image=music,text="Currently playing",bg="white",  fg="white", anchor=E, font=("Helvetica",12,"bold", "italic"))
