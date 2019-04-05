41041fea739f25d11e2d37eac491290a932df676
Kristoffer Vollset <kriss_vollset@hotmail.com>
2019-04-05 11:57:15 -0400

    Update RPS_GUI.py

--- RPS_GUI.py
+++ RPS_GUI.py
@@ -18,6 +18,8 @@ gameWindow=1;
 winnerWindow=1;
 name=0;
 singlePlayer=False;
+set_up_window=1
+number_of_players=True
         
         
         
@@ -57,6 +59,7 @@ def restart():
         global Team_One_Index
         global Team_Two_Index
         global singlePlayer
+        global startWindow
         counter_One=1
         Team_One_Index=0
         Team_Two_Index=0
@@ -77,6 +80,7 @@ def restart():
         e.pack()
         clickToPlay = Button(startWindow, text='Play!', width=8, font='Bizon 20 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=lambda:doStuff(startWindow))
         clickToPlay.place(x=75, y=125)
+        startWindow.bind('<Return>',enter)
         startWindow.mainloop()
 
 
@@ -167,10 +171,13 @@ def settingUpTheGame(set_up_window):
         gameWindow.config(background='White')
         rock = Button(gameWindow, text='Rock', width=8, font='Bizon 12 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=theyAreWantRock)
         rock.place(x=0, y=125)
+        gameWindow.bind('<Left>',rockKey)
         paper = Button(gameWindow, text='Paper', width=8, font='Bizon 12 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=theyAreWantPaper)
         paper.place(x=100, y=125)
+        gameWindow.bind('<Down>',paperKey)
         scissor = Button(gameWindow, text='Scissors', width=8, font='Bizon 12 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=theyAreWantScissor)
         scissor.place(x=200, y=125)
+        gameWindow.bind('<Right>',scissorKey)
         TeamOne.reverse()
         TeamTwo.reverse()
         computer_One_choice = random.randint(1,3)
@@ -190,6 +197,14 @@ def settingUpTheGame(set_up_window):
         #else:Final_Team_Two_Choice = computer_Two_choice"""
         else:
                 winner()
+
+def rockKey(_event):
+        theyAreWantRock()
+def paperKey(_event):
+        theyAreWantPaper()
+def scissorKey(_evemt):
+        theyAreWantScissor()
+        
 
 def winner():
         global gameWindow
@@ -243,21 +258,29 @@ def nameTeamGetProcess(set_up_window,number_of_players,counter_One, startWindow,
                         TeamTwo.append(name_of_player)
                         global Team_Two_Index
                         Team_Two_Index += 1
-        if counter_One <= number_of_players:
+                        
+        if counter_One <= number_of_players and singlePlayer:
+                label_1['text'] = "Please enter your name Player {}\nthen click play: ".format(counter_One)
                 #.set("Please enter your name Player {}:".format(counter_One))
                 #need to define raw_input
                 #name_of_player = raw_input('Please enter your name: ')
+        elif counter_One <= number_of_players:
                 label_1['text'] = "Please enter your name Player {}\nthen click the team you want to join: ".format(counter_One)
         if counter_One > number_of_players:     
                 settingUpTheGame(set_up_window) 
 
 #This problem got fixed by letting v be a parameter in the function as well. 
 def doStuff(startWindow):
-        #Close [startWindow] before advancing:
+        global set_up_window
+        global number_of_players
         global v
         s = v.get()
-        number_of_players = int(s)
-        
+        try:
+                number_of_players = int(s)
+        except ValueError:
+                print("Must type in an integer!")
+
+        #Close [startWindow] before advancing:
         startWindow.destroy()
         startWindow.quit()
         if number_of_players==0:
@@ -282,24 +305,42 @@ def doStuff(startWindow):
         label_set_Up=Label(set_up_window, text='Welcome to the set up!', font='Bizon 20 bold', bg='PeachPuff2')
         label_set_Up.pack(side=TOP,fill=X)
         global label_1
-        label_1=Label(set_up_window,text='Please enter your name player 1,\nthen click the team you want to join: ', font='Bizon 15 bold', bg='PeachPuff2')
+        if(number_of_players==1):
+                label_1=Label(set_up_window,text='Please enter your name player 1,\nthen click play!', font='Bizon 15 bold', bg='PeachPuff2')
+        else:
+                label_1=Label(set_up_window,text='Please enter your name player 1,\nthen click the team you want to join: ', font='Bizon 15 bold', bg='PeachPuff2')
         label_1.pack()
+        global e
         v = StringVar()
         e = Entry(set_up_window, textvariable=v)
+        e.focus_set()
         e.pack()
         if (number_of_players==1):
                 global singlePlayer
                 singlePlayer=True
                 clickStart = Button(set_up_window, text="Let's play", width=8, font='Bizon 15 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=lambda:theyWantTeamOne(set_up_window,number_of_players))
+                set_up_window.bind('<Return>',letsPlay)
                 clickStart.place(x=125, y=150)
                 mainloop()
         else :
                 clickForTeamOne = Button(set_up_window, text='Team 1', width=8, font='Bizon 20 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=lambda:theyWantTeamOne(set_up_window,number_of_players))
                 clickForTeamOne.place(x=5, y=125)
+                set_up_window.bind('<Left>',teamOne)
                 clickForTeamTwo = Button(set_up_window, text='Team 2', width=8, font='Bizon 20 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=lambda:theyWantTeamTwo(set_up_window,number_of_players))
                 clickForTeamTwo.place(x=150, y=125)
+                set_up_window.bind('<Right>',teamTwo)
                 #nameTeamGetProcess(number_of_players,counter_One,set_up_window,0)
                 mainloop()
+
+def enter(_event):
+        global startWindow
+        doStuff(startWindow)
+def letsPlay(_event):
+        theyWantTeamOne(set_up_window,number_of_players)
+def teamOne(_event):
+        theyWantTeamOne(set_up_window,number_of_players)
+def teamTwo(_event):
+        theyWantTeamTwo(set_up_window,number_of_players)
         
 
 
@@ -313,7 +354,9 @@ lblEntry = Label(text="Please input the number of players: ",bg="black", fg="whi
 e = Entry(startWindow,textvariable=v)
 lblEntry.pack()
 e.pack()
+e.focus_set()
 clickToPlay = Button(startWindow, text='Play!', width=8, font='Bizon 20 bold', bg='Black', fg='Yellow', relief=RIDGE, bd=0, command=lambda:doStuff(startWindow))
+startWindow.bind('<Return>',enter)
 clickToPlay.place(x=75, y=125)
 
 
